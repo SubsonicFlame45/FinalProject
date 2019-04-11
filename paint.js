0cf08505e80a5155945360626f07280f339bd4ba
Sergio Almonte <sergioealmonte@gmail.com>
2019-04-10 17:06:57 -0700

    added the color printing to console.

--- paint.js
+++ paint.js
@@ -9,8 +9,8 @@ let draw = SVG('drawing').size(canvasWidth, canvasHeight);
 
 const WHITE = '#fff';
 const BLACK = '#000';
-
-for (let i = 0; i <numRows; i++ ){
+let currentColor = '#000';
+for (let i = 0; i < numRows; i++ ){
     let rowY = i * blockHeight;
 
     for (let j = 0; j < numBlocks; j++) {
@@ -26,10 +26,46 @@ for (let i = 0; i <numRows; i++ ){
     rect.element.fill(WHITE);
     }
 }
-
+document.getElementById("black").onclick = function() {
+    currentColor = '#000'
+    console.log(currentColor)
+}
+document.getElementById("white").onclick = function() {
+    currentColor = '#fff'
+    console.log(currentColor)
+}
+document.getElementById("red").onclick = function() {
+    currentColor = '#ff0000'
+    console.log(currentColor)
+}
+document.getElementById("black").onclick = function() {
+    currentColor = '#000'
+    console.log(currentColor)
+}
+document.getElementById("orange").onclick = function() {
+    currentColor = '#ffa500'
+    console.log(currentColor)
+}
+document.getElementById("yellow").onclick = function() {
+    currentColor = '#ffff00'
+    console.log(currentColor)
+}
+document.getElementById("green").onclick = function() {
+    currentColor = '#008000'
+    console.log(currentColor)
+}
+document.getElementById("blue").onclick = function() {
+    currentColor = '#0000ff'
+    console.log(currentColor)
+}
+document.getElementById("violet").onclick = function() {
+    currentColor = '#ee82ee'
+    console.log(currentColor)
+}
+function colorUpdate()  {
+    console.log(currentColor)
+}
 function colorFunction() {
+    
     console.log("this works");
-
 } 
-document.getElementById("black").onclick = COLOR = '#000';
-document.getElementById("white").onclick 
